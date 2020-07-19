1292df3c0cba27d8f9176f9fbaca7b5f07d21a5e
Hussam <halshehri@deakin.edu.au>
2020-07-20 00:57:04 +1000

    create biosensor.js file

---
+++ sit725-prac1/biosensor.js
@@ -0,0 +1,33 @@
+
+  class Biosensor
+  {
+     constructor(name)
+     {
+            this.owner=name;
+            this.hearrate=0;
+     }
+
+     generatRandomBetween(min ,max)
+     {
+
+        return parseInt(math.random() * (max -min ) + min );
+     }
+
+    beat()
+    {
+      let hr=this.generatRandomBetween(70,80)
+      this.heartRat=hr;
+      console.log('[owner]:'+ this.owner + '---- [HR] '+ this.heartRate)
+    }
+
+    live()
+    {
+      
+        setInterval(this.beat,1000)
+
+    }
+  }
+  
+
+  let james= new Biosensor("james")
+  console.log(james)
